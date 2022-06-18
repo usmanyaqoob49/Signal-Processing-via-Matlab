e007c2e101c0d7d90aae812035e187d36fb0865c
Usman Yaqoob <68185452+usmanyaqoob49@users.noreply.github.com>
2022-06-18 11:06:09 +0500

    Add files via upload

---
+++ Different Signals Generation and Plotting/2. Generating_and_plotting_anticausal_signal.m
@@ -0,0 +1,21 @@
+clc
+clear all
+close all
+t=-2:1/20:2;
+x1 = sin(2*pi*2*t);
+subplot(3,1,1);
+stem(t,x1,'LineWidth',2);
+xlabel('time');
+ylabel('signal amplitude');
+title('sin(2*\pi*f*t)');
+u = (t<0);
+x2 = x1.*u;
+subplot(3,1,2);
+stem(t,u, 'r');
+xlabel('time');
+ylabel('Signal Amplitude');
+title('Unit Step');
+subplot(3,1,3);
+stem(t,x2, 'k','LineWidth',2);
+xlabel('time'); ylabel('signal amplitude');
+title('causal version of sin(2*\pi*f*t)');
